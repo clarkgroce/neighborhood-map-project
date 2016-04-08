45db4762701af4c33d3c5cd60542d56496c6a709
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-04-08 17:46:14 -0400

    added css file and also started work on filterable list based on foursquare data

--- LocationListViewModel.js
+++ LocationListViewModel.js
@@ -1,20 +1,56 @@
+(function () {
+    'use strict';
 
-var LocationListModel = function(items) {
-    this.items = ko.observableArray(items);
-    this.itemToAdd = ko.observable("");
+    // Represent a single location
+    var Location = function (title, lat, lng) {
+        this.title = title;;
+        this.lat = lat;
+        this.lng = lng;
+    };
+    // The Location List ViewModel
+    var LocationListViewModel = function (locations) {
+        // Array of passed in locations -- mapped to an observableArray of Location objects
+       this.locations = ko.observableArray(locations.map(function (location) {
+           return new Location(location.title, location.lat, location.lng);
+        }));
 
-    this.addItem = function() {
-        if (this.itemToAdd() != "") {
-            this.items.push(this.itemToAdd());
-            this.itemToAdd("");
-        }
-    }.bind(this);
+        // Store the current search filter entered by the user
+        this.currentFilter = ko.observable();
 
-    this.selectedItems = ko.observableArray(["Second"]);
+        // Filter locations array based on search input
+        this.filteredLocations = ko.computed(function () {
+            if (!this.currentFilter()) {
+                return this.locations();
+            } else {
+                return ko.utils.arrayFilter(this.locations(), function(location) {
+                    return location.title.indexOf(this.currentFilter()) > -1;
+                }.bind(this));
+            }
+        }.bind(this));
+        this.filter = function() {
+            this.currentFilter();
+        };
+        //this.selectedItems = ko.observableArray(["Second"]);
+ 
+        //this.sortItems = function() {
+        //    this.items.sort();
+        //}.bind(this);
+    };
+    // Load example data from FourSquare
+    var queryURL = 'https://api.foursquare.com/v2/venues/search?near="Vancouver, BC"&oauth_token=XWDKSEKZ0FTNFJMOJ1SA5MSSA1HZVCMPTTZ5DYJUX0YFI3K4&v=20150509';
 
-    this.sortItmes = function() {
-        this.items.sort();
-    }.bind(this);
-};
+     var locations = [];
+ 
+     $.getJSON(queryURL, function( data ) {
+                 var venues = data.response.venues;
+ 
+         for (var i = 0; i < venues.length; i++) {
+            locations.push(new Location(venues[i].name, venues[i].location.lat, venues[i].location.lng));
+         }
 
-ko.applyBindings(new LocationListModel(["First", "Second", "Third"]));
+        console.log(locations);
+        // Bind an instance of our viewModel to the page
+        var viewModel = new LocationListViewModel(locations || []);
+        ko.applyBindings(viewModel);
+     });
+}());
