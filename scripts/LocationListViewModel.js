86147e37fe19e5a01101e1943ddfb44ba835a492
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-04-12 00:10:26 -0400

    fixed bugs in location window and added click handler

--- scripts/LocationListViewModel.js
+++ scripts/LocationListViewModel.js
@@ -12,22 +12,22 @@
         self.lng = lng;
         self.log = function() {
             console.log(self);
-        }
+        };
 
         self.addToMap = function(googleMap) {
-            //marker creation
+            // Create a marker
             self.marker = new google.maps.Marker({
                 position: new google.maps.LatLng(self.lat,self.lng),
                 map: googleMap,
                 title: self.title
             });
 
-            var InfoWindow = new google.maps.InfoWindow({ content: self.info});
+            var infowindow = new google.maps.InfoWindow({ content: self.info});
 
             google.maps.event.addListener(self.marker, 'click', function() {
                 infowindow.open(googleMap, self.marker);
             });
-        }
+        };
     };
 
 
@@ -97,7 +97,7 @@
         //Load data from foursquare
         function loadFourSquareData() {
             // Return the top interesting results from FourSquare
-            var queryURL = 'https://api.foursquare.com/v2/venues/explore?ll=34.07,-84.52&client_id=PVACF2UV50T3NPRAN10QREVQGUFATZXJ23K01UJ1GZB4T2K0&client_secret=42PTYNAYSSEO1WZDYRRIWFOVA442LHB10FPFM12BSM43ZSM5&v=20160410';
+            var queryURL = 'https://api.foursquare.com/v2/venues/explore?ll=34.102,-84.519&client_id=PVACF2UV50T3NPRAN10QREVQGUFATZXJ23K01UJ1GZB4T2K0&client_secret=42PTYNAYSSEO1WZDYRRIWFOVA442LHB10FPFM12BSM43ZSM5&v=20160410';
 
             $.getJSON(queryURL, function(data) {
                 //console.log(data);         
@@ -124,7 +124,7 @@
                 '</div>'+
                 '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>'+
                 '<div id="bodyContent">'+
-                '<p>phoneNumber</p>'+
+                '<p>' + phoneNumber + '</p>'+
                 '</div>'+
                 '</div>';
             var lat = locationData.location.lat;
@@ -162,6 +162,9 @@
             self.currentFilter();
         };
 
+        self.searchResultsClicked = function(location) {
+            console.log(location);
+        };
 
         // Center and resize map when window resized
         window.addEventListener('resize', function() {
